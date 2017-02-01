0c8a3f6e1da9ce1aeedc62a334d5de208453c9fd
Sergey Bodnaryuk <bodnaryuk_s.a@hotmail.com>
2017-02-01 23:47:18 +0300

    Исправляет код в соответствии с замечаниями

--- js/setup.js
+++ js/setup.js
@@ -16,40 +16,39 @@ var wizardCoat = document.querySelector('#wizard-coat');
 var wizardEyes = document.querySelector('#wizard-eyes');
 var setupFireballWrap = document.querySelector('.setup-fireball-wrap');
 
-var randomNumber = function (number) {
-  return Math.floor(Math.random() * number.length);
+var randomNumber = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
 
+var coatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var eyesColors = [
+  'black',
+  'red',
+  'yellow',
+  'blue',
+  'green'
+];
+var fireballColors = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
 wizardCoat.addEventListener('click', function () {
-  var coatColors = [
-    'rgb(101, 137, 164)',
-    'rgb(241, 43, 107)',
-    'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)',
-    'rgb(215, 210, 55)',
-    'rgb(0, 0, 0)'
-  ];
-  wizardCoat.style.fill = coatColors[randomNumber(coatColors)];
+  wizardCoat.style.fill = randomNumber(coatColors);
 });
-
 wizardEyes.addEventListener('click', function () {
-  var eyesColors = [
-    'black',
-    'red',
-    'yellow',
-    'blue',
-    'green'
-  ];
-  wizardEyes.style.fill = eyesColors[randomNumber(eyesColors)];
+  wizardEyes.style.fill = randomNumber(eyesColors);
 });
-
 setupFireballWrap.addEventListener('click', function () {
-  var fireballColors = [
-    '#ee4830',
-    '#30a8ee',
-    '#5ce6c0',
-    '#e848d5',
-    '#e6e848'
-  ];
-  setupFireballWrap.style.background = fireballColors[randomNumber(fireballColors)];
+  setupFireballWrap.style.background = randomNumber(fireballColors);
 });
